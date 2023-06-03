9bc1c80444cffb05610c57eeb3a9f6a0e673abcd
FilipNeubauer <filipneub@seznam.cz>
2023-06-03 08:59:45 +0200

    intro page + scroll down

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,35 @@
 import React from 'react';
 import "../styles.css"
-import Logo from './Logo';
+import { useState } from 'react';
+import but from "../images/buttondown1.svg"
+import ready from "../images/ready1.png"
+import logo from "../images/r3master.png"
+
 
 const App = () => {
-    return (<div className='backG'>
-        <Logo />
+    const [shifted, setShifted] = useState(false);
+
+    const shiftFoo = () => {
+        const targetElementBody = document.getElementsByTagName("body")[0];
+        const targetElementHtml = document.getElementsByTagName("html")[0];
+
+        targetElementBody.style.overflowY = "scroll";
+        targetElementHtml.style.overflowY = "scroll";
+
+        setShifted(!shifted)
+    }
 
 
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div id="here">asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div><div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
-<div>asdkasd</div>
+    return (<div className={`backG ${shifted ? "shifted" : ""}`}>
+      <div className='introPage'>
+        <img src={logo} className='logo' alt='Remaster'/>
+        <img src={ready} className='ready' alt='ready for tomorrow?'/>
+        <input type="image" onClick={shiftFoo} src={but} className='buttonDown svg-image' alt='button down'/>
+
+      </div>
 
 
-    </div>)
+</div> )
 }
 
 export default App
