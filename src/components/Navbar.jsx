0971c1bb79d8ba67c04d04936495a3482786f38a
Jakub Rehak <r3hyx@R3-MacBook-Air.local>
2023-07-09 11:09:42 +0200

    m

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "../styles.css"
 import miniLogo from "../images/r3.png"
+import { useState } from 'react';
 
 
 function Navbar() {
+
+  // if on mobile
+  const [hamburger, setHamburger] = useState(true)
+
+  // open or not
+  const [open, setOpen] = useState(false)
+
+
+
+
   return (
     <>
     <div className='wholeBar'>
@@ -37,6 +48,40 @@ function Navbar() {
               <Link to='/Projects'>Projects</Link>
             </li> */}
           </ul>
+
+
+
+          {/* hamburger navbar */}
+          <ul className='hamburgerNavList'>
+            <li>
+                <Link to='/About'>About Us</Link>      
+              </li>
+
+              <li>
+                <Link to='/Contact'>Contact</Link>
+              </li>
+
+              <Link to='/'>
+                <input className="miniLogo" type="image" src={miniLogo}/>
+              </Link>
+
+              <li>
+                <Link to='/Pricing'>Pricing</Link>
+              </li>
+
+              <li>
+                <Link to='/Services'>Services</Link>
+              </li>
+          </ul>
+ 
+            <div className={`hamburgerContainer ${open ? 'open' : ''}`} onClick={() => setOpen(!open)}>
+                <div className='line'></div>
+                <div className='line'></div>
+                <div className='line'></div>
+            </div> 
+
+
+
         </nav>
 
     </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import "../styles.css"
 import Navbar from '../components/Navbar';
+import neub from "../images/neubauerf.png"
+import reh from "../images/rehakj.png"
 
 const Contact = () => {
     
     return (
+        <>
         <Navbar/>
+        <div className='contentContact'>
+        <div className='contact'>
+            <h2>Contact Us</h2>
+        <p> 
+            Did we catch your attention? Do not hesitate and contact us
+            right now! 
+        </p>
+        <div>
+            {/* <img src={reh} className='member1'/>
+            <img src={neub} className='member1'/> */}
+        </div>
+        </div>
+        </div>
+        </>
 )
 }
 
