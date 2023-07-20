06230940a6511e0beac9d8536bfee39063e61ed8
Jakub Rehak <r3hyx@R3-MacBook-Air.local>
2023-07-20 16:53:49 +0200

    pictures replaced with text

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,6 +8,7 @@ import digital from "../images/digitalsolutions.png"
 import { Link, useNavigate } from 'react-router-dom';
 import miniLogo from "../images/WhiteR3.png"
 import Footer from '../components/Footer';
+import dig from "../images/digitalFrame.svg"
 
 
 const MainPage = () => {
@@ -164,7 +165,14 @@ const MainPage = () => {
 
       <div className='contentFirstPage'>
 
-      <img  src={digital} className='digital' alt='Digital Solutions for You'/>
+      
+        <div className="digital-container">
+        <div className="overlay-Text">
+          <h3>Digital Solutions Tailored for You!</h3>
+        </div>
+        {/* <img src={dig} className="digital" alt="Digital Solutions for You" /> */}
+      </div>
+    
 
         <button onClick={handleClick} className='startBtn'>
           Start Your Journey
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -12,7 +12,28 @@ const Services = () => {
         <div className='servicesContainer'>
         <Navbar current={"services"}/>
         <div className='contentServices'>
-            <img  src={servimg} className='servicesimg' alt='Services'/>
+            {/* <img  src={servimg} className='servicesimg' alt='Services'/> */}
+
+        <div className="services-layer">
+            <div className="software-dvelopment">
+            <h2>Software Development</h2>
+            <p className='sd-text'>Whether you need a robust web application, a scalable 
+mobile app, or a complex enterprise 
+software system, we got you covered.
+            </p>
+            </div>
+
+            <div className="design">
+            <h2>Design</h2>
+            <p className='design-text'>Creativity. Minimalism. Effectivity.
+These are the only words we need to describe 
+what we mean by graphical design. Logo, user 
+interface of your app, or a poster, everything 
+will look exceptionally good.
+            </p>
+            </div>
+        </div>
+
         </div>
         <Footer />
         </div>
